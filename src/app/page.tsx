dcd86a96d6a07c09588b4ab1001a2445785760f8
abhitrueprogrammer <abhiaruvs3@gmail.com>
2025-06-24 13:49:57 +0530

    remove unnessasary imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import Navbar from "@/components/navbar";
-import { DataTable } from "@/components/table/data-table";
-import type {
-  VisibilityState,
-  SortingState,
-  ColumnFiltersState,
-} from "@tanstack/react-table";
-import type { SetStateAction } from "react";
+
 
 export default function HomePage() {
   return (
